56850cd52483c7c36df2f6e9ef532d64a6a41777
captainQ <lordkuvin@gmail.com>
2022-03-23 10:19:12 +0600

    Создал echo view в приложения "Api_v1"

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,20 @@
+import json
+from datetime import datetime
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def echo_view(request):
+    response_data = {
+        "method": request.method,
+        "datetime": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    }
+
+    print(request.body)
+    if request.body:
+        response_data["body"] = json.loads(request.body)
+
+    response = JsonResponse(response_data)
+
+    return response
